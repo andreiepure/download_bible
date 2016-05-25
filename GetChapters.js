560ee8fc21ee47ecd5cf34ef3cee34fe457f6946
Andrei Epure <andrei.epure@cti.pub.ro>
2016-05-26 00:10:28 +0100

    Slava Domnului, am adaugat logica pentru parsarea capitolelor. Trebuie doar sa le inserez, cu mila Domhului!

--- GetChapters.js
+++ GetChapters.js
@@ -27,18 +27,21 @@ db.serialize(function() {
 	// db.run("CREATE TABLE Books (test_id INTEGER NOT NULL, s_name TEXT NOT NULL, l_name TEXT NOT NULL, path TEXT NOT NULL)");
 	db.each("SELECT rowid, test_id, s_name, l_name, path FROM Books", function(err, row) {
 		var currentBook = new Book(row.rowid, row.s_name, row.l_name, row.path);
+		books.push(currentBook);
+
+		/*
+		var debugIds = [24];
+		if (debugIds.indexOf(currentBook.bookId) == -1)
+		{
+			return;
+		}
+		*/
+
 		console.log("Current book " + currentBook.bookId + " " + 
 			currentBook.testamentId +" "+
 			currentBook.shortName +" "+
 			currentBook.longName +" "+
 			currentBook.relativePath + " " + currentBook.url);
-		books.push(currentBook);
-
-		var debug_firstBook_in_newTestament = 54;
-		if (currentBook.bookId >= debug_firstBook_in_newTestament)
-		{
-			return;
-		}
 
 		var res = request('GET', currentBook.url);
 		var body;
@@ -51,7 +54,6 @@ db.serialize(function() {
 			return;
 		}
 
-
 		var title = $('body>div');
 
 		// fiecare rand are doua celule: prescurtarea si numele cu legatura
@@ -61,8 +63,33 @@ db.serialize(function() {
 			var row = rows[i];
 			var link = row.children[1].children[0];
 
+			debugger;
 			var shortName = $(row.children[0]).text();
-			var longName = link.children[0].data;
+
+			var intelept = (shortName == " - " && currentBook.shortName == "Sir");
+			if (intelept)
+			{
+				shortName = "Cap. 0";
+			}
+
+			var longName;
+			var exceptions = ["Ps", "Cant", "Ir"];
+			if (link.children.length == 1 ||
+				exceptions.indexOf(currentBook.shortName) != -1 ||
+				intelept)
+			{
+				longName = link.children[0].data;
+			}
+			else if (link.children.length == 3)
+			{
+				longName = link.children[2].data;
+			}
+			else if (link.children.length == 5)
+			{
+				// la psalmi
+				longName = link.children[2].data + link.children[4].data;
+			}
+
 			var relativePath = link.attribs.href;
 
 			console.log(shortName + " - " + longName + " - " + relativePath);
