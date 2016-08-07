f5afbd882a7dabea7b0a61c015d8bde61a8a26a0
Andrei Epure <epure.andrei@gmail.com>
2016-08-07 23:50:05 +0100

    inserting everything at the end after parsing; the chapter querying is sequential so it kinda works; previously I was encountering locked db errors because of the db being opened in the GUI, I think...

--- ParseLocalVersets.js
+++ ParseLocalVersets.js
@@ -1,14 +1,3 @@
-var request = require('sync-request');
-var cheerio = require('cheerio');
-var URL = require('url-parse');
-var sqlite3 = require('sqlite3').verbose();
-var fs = require('fs');
-var mkdirp = require('mkdirp');
-
-var file = "bible.db";
-var exists = fs.existsSync(file);
-
-
 function Chapter(chapterId, bookId, number, title, path, bookShortName)
 {
 	this.chapterId = chapterId;
@@ -29,16 +18,13 @@ function Verset(chapterId, number, text)
 
 // IMPORTANT
 // for Note and Link, the versetId is not present at parse time.
-// we insert the notes and links once with each verset, thus it is
-// not necessary to keep the verset note in the Note and Link object
-// at this level. It will be in the consumer application, because
-// we do insert the versetId in the database Notes and Versets tables
 
 // An addnotation (explanatory note) for a certain verset. The HTML is kept
 // as original (it will have external links to the Dervent Monastery website)
-function Note(letter, text)
+function Note(chapterId, versetNumber, letter, text)
 {
-	//this.versetId = versetId;
+	this.chapterId = chapterId;
+	this.versetNumber = versetNumber;
 	this.letter = letter;
 	this.text = text;
 }
@@ -46,16 +32,18 @@ function Note(letter, text)
 // A link to one or more versets in the Scripture - usually versets have multiple,
 // especially in the New Testament.
 // In the DB, the start and end verset ids are kept in order to keep the DB normalized
-function Link(targetBook, targetChapter, startVerset, endVerset)
+function Link(chapterId, versetNumber, targetBookShortName, targetChapter, startVerset, endVerset)
 {
 	//this.versetId = versetId;
-	this.targetBook = targetBook;
+	this.chapterId = chapterId;
+	this.versetNumber = versetNumber;
+
+	this.targetBookShortName = targetBookShortName;
 	this.targetChapter = targetChapter;
 	this.targetStartVerset = startVerset;
 	this.targetEndVerset = (endVerset === undefined) ? startVerset : endVerset;
 }
 
-var db = new sqlite3.Database(file);
 
 function RetrieveNote(noteRelativeUrl)
 {
@@ -76,6 +64,7 @@ function RetrieveNote(noteRelativeUrl)
 	return text;
 }
 
+/*
 function GetVersetId(bookShortName, chapterNumber, versetNumber)
 {
 	var statement =
@@ -92,95 +81,96 @@ function GetVersetId(bookShortName, chapterNumber, versetNumber)
 
 	return versetId;
 }
-
+*/
 function ProcessRows(file, $, rows, chapterId)
 {
 	var db = new sqlite3.cached.Database(file);
-	db.serialize(function() {
-		for (var i = 0; i < rows.length; i++) {
-			var row = rows[i];
+	var versetNotes = [];
+	var versetLinks = [];
+	var versets = [];
 
-			// needed to insert
-			var versetNumber = $(row.children[0].children[1]).text();
-			var versetText;
-			var versetNotes = [];
-			var versetLinks = [];
+	for (var i = 0; i < rows.length; i++) {
+		var row = rows[i];
 
-			// needed to construct
-			var content = row.children[1];
-			var versetTextTokens = [];
+		// needed to insert
+		var versetNumber = $(row.children[0].children[1]).text();
+		var versetText;
 
-			content.children.forEach(function(childElement) {
-				if (childElement.name === undefined) {
-					versetTextTokens.push($(childElement).text().trim());
+		// needed to construct
+		var content = row.children[1];
+		var versetTextTokens = [];
+
+		content.children.forEach(function(childElement) {
+			if (childElement.name === undefined || childElement.name ==='b' || childElement.name ==='i' ) {
+				versetTextTokens.push($(childElement).text().trim());
+			}
+			else {
+				if (childElement.name === 'sup') {
+					var href = childElement.children[0].attribs.href
+					var noteLetter = $(childElement).text()
+					var text = RetrieveNote(href);
+					var note = new Note(chapterId, versetNumber, noteLetter, text);
+					versetNotes.push(note);
+				}
+				else if (childElement.name === 'div') {
+					childElement.children.forEach(function(versetLink) {
+						var linkContent = $(versetLink).text();
+						var pieces = linkContent.split(' ');
+						if (pieces.length == 2) {
+							var bookAndVersets = pieces[1].split(':');
+							var versets = bookAndVersets[1].split('-');
+							var bookShortName = pieces[0];
+							var chapter = bookAndVersets[0];
+							var firstVerset = versets[0];
+							var lastVerset = versets[1];
+							var link = new Link(chapterId, versetNumber, bookShortName, chapter, firstVerset, lastVerset);
+							versetLinks.push(link);
+						}
+					});
 				}
 				else {
-					if (childElement.name === 'sup') {
-						var href = childElement.children[0].attribs.href
-						var noteLetter = $(childElement).text()
-						var text = RetrieveNote(href);
-						var note = new Note(noteLetter, text);
-						versetNotes.push(note);
-					}
-					else if (childElement.name === 'div') {
-						childElement.children.forEach(function(versetLink) {
-							var linkContent = $(versetLink).text();
-							var pieces = linkContent.split(' ');
-							if (pieces.length == 2) {
-								var bookAndVersets = pieces[1].split(':');
-								var versets = bookAndVersets[1].split('-');
-								var bookShortName = pieces[0];
-								var chapter = bookAndVersets[0];
-								var firstVerset = versets[0];
-								var lastVerset = versets[1];
-								var link = new Link(bookShortName, chapter, firstVerset, lastVerset);
-								versetLinks.push(link);
-							}
-						});
-					}
-					else {
-						console.log("Unrecognized childElement name in chapter row: " + childElement.name);
-					}
+					console.log("Unrecognized childElement name in chapter row: " + childElement.name);
 				}
-			});
+			}
+		});
 
-			versetText = versetTextTokens.join(' ');
+		versetText = versetTextTokens.join(' ');
+		var verset = new Verset(chapterId, versetNumber, versetText);
+		versets.push(verset);
 
-			// insert the verset
+	}
 
-			console.log("Will insert "+chapterId + " " +versetNumber+" "+versetText);
-
-			var stmt = db.prepare("INSERT INTO Versets VALUES (?, ?, ?)");
-			stmt.run(chapterId, versetNumber, versetText);
-			stmt.finalize();
-
-			debugger;
-
-			// retrieve the verset unique id
-			//	var versetId;
-			//		var selectStatement = "SELECT rowid AS id FROM Versets WHERE chap_id = " + chapterId + " AND number = " + versetNumber;
-
-			var noteStatement = db.prepare("INSERT INTO TemporaryNotes VALUES (?, ?, ?, ?)");
-			debugger;
-			versetNotes.forEach(function(note) {
-					noteStatement.run(chapterId, versetNumber, note.letter, decodeURIComponent(note.text));
-			});
-			noteStatement.finalize();
-
-			var linkStatement = db.prepare("INSERT INTO TemporaryLinks VALUES (?, ?, ?, ?, ?, ?)");
-			debugger;
-			versetLinks.forEach(function(link) {
-				// get the start verset id
-				//var startVersetId = GetVersetId(link.targetBook, link.targetChapter, link.targetStartVerset);
-				//var endVersetId = GetVersetId(link.targetBook, link.targetChapter, link.targetEndVerset);
-				linkStatement.run(chapterId, versetNumber, link.targetBook, link.targetChapter, link.targetStartVerset, link.targetEndVerset);
-			});
-
-			linkStatement.finalize();
-		}
+	var stmt = db.prepare("INSERT INTO Versets VALUES (?, ?, ?)");
+	versets.forEach(function(verset) {
+		stmt.run(verset.chapterId, verset.number, verset.text);
 	});
+	stmt.finalize();
+
+	var noteStatement = db.prepare("INSERT INTO TemporaryNotes VALUES (?, ?, ?, ?)");
+	versetNotes.forEach(function(note) {
+			noteStatement.run(note.chapterId, note.versetNumber, note.letter, decodeURIComponent(note.text));
+	});
+	noteStatement.finalize();
+
+	var linkStatement = db.prepare("INSERT INTO TemporaryLinks VALUES (?, ?, ?, ?, ?, ?)");
+	versetLinks.forEach(function(link) {
+		linkStatement.run(link.chapterId, link.versetNumber, link.targetBookShortName, link.targetChapter, link.targetStartVerset, link.targetEndVerset);
+	});
+	linkStatement.finalize();
+
+	console.log("Inserted for "+chapterId);
 }
 
+var request = require('sync-request');
+var cheerio = require('cheerio');
+var URL = require('url-parse');
+var sqlite3 = require('sqlite3').verbose();
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+
+var file = "bible.db";
+var exists = fs.existsSync(file);
+var db = new sqlite3.Database(file);
 
 db.serialize(function() {
 	db.each("SELECT C.rowid, C.book_id, C.number, C.title, C.path, B.s_name FROM Chapters AS C INNER JOIN Books AS B ON C.book_id = B.rowid", function(err, row) {
@@ -206,10 +196,11 @@ db.serialize(function() {
 
 		//var title = $('body>div');
 
-
 		var rows = $('body>table>tr');
+		debugger;
 		ProcessRows(file, $, rows, currentChapter.chapterId);
-	})
+})
 });
 	
 db.close();
+
