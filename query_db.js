5075984b3afb7f58bbe6f8faa36c4b0344daf6f3
Andrei Epure <epure.andrei@gmail.com>
2017-08-21 23:32:22 +0200

    updated db schema

--- query_db.js
+++ query_db.js
@@ -13,39 +13,39 @@ var db = new sqlite3.Database(file);
 
 db.serialize(function() {
 
-	db.each("SELECT rowid, s_name, l_name, path FROM Testaments", function(err, row) {
+	db.each("SELECT rowid AS id, shortName, longName FROM Testaments", function(err, row) {
 		if (row)
-			console.log(row.rowid + ": " + row.s_name + ": " + row.l_name + ": " + row.path);
+			console.log(row.id + ": " + row.shortName + ": " + row.longName);
 	});
 
-	db.each("SELECT  rowid, test_id, s_name, l_name, path FROM Books WHERE test_id = 1 LIMIT 5", function(err, row) {
+	db.each("SELECT rowid AS id, testamentId, shortName, longName FROM Books WHERE testamentId = 1 LIMIT 5", function(err, row) {
 		if (row)
-		console.log(row.rowid + ": " + row.test_id + ": " + row.s_name + ": " + row.l_name + ": " + row.path);
+		console.log(row.id + ": " + row.testamentId + ": " + row.shortName + ": " + row.longName);
 	});
 
-	db.each("SELECT  rowid, book_id, number, title, path FROM Chapters WHERE book_id = 1 LIMIT 5", function(err, row) {
+	db.each("SELECT rowid AS id, bookId, number, title FROM Chapters WHERE bookId = 1 LIMIT 5", function(err, row) {
 		if (row)
-		console.log(row.rowid + ": " + row.book_id + ": " + row.number + ": " + row.title + ": " + row.path);
+		console.log(row.id + ": " + row.bookId + ": " + row.number + ": " + row.title);
 	});
 
-	db.each("SELECT  rowid, chap_id, number, text FROM Versets WHERE chap_id = 1 LIMIT 5", function(err, row) {
+	db.each("SELECT rowid AS id, chapterId, number, text FROM Versets WHERE chapterId = 1 LIMIT 5", function(err, row) {
 		if (row)
-		console.log(row.rowid + ": " + row.chap_id + ": " + row.number + ": " + row.text);
+		console.log(row.id + ": " + row.chapterId + ": " + row.number + ": " + row.text);
 	});
 
-	db.each("SELECT  rowid, verset_id, letter, text FROM Notes WHERE verset_id = 1 LIMIT 5", function(err, row) {
+	db.each("SELECT rowid AS id, versetId, letter, text FROM Notes WHERE versetId = 1 LIMIT 5", function(err, row) {
 		if (row)
-		console.log(row.rowid + ": " + row.verset_id + ": " + row.letter + ": " + row.text);
+		console.log(row.id + ": " + row.versetId + ": " + row.letter + ": " + row.text);
 	});
 
-	db.each("SELECT  rowid, source_verset_id, dest_chapter_id, dest_start_verset_number, dest_end_verset_number FROM Links LIMIT 5", function(err, row) {
+	db.each("SELECT rowid AS id, sourceVersetId, targetChapterId, targetVersetNumberStart, targetVersetNumberEnd FROM Links LIMIT 5", function(err, row) {
 		if (row)
-		console.log(row.rowid + ": " + row.source_verset_id + ": " + row.dest_chapter_id + ": " + row.dest_start_verset_number  + ": " + row.dest_end_verset_number);
+		console.log(row.id + ": " + row.sourceVersetId + ": " + row.targetChapterId + ": " + row.targetVersetNumberStart  + ": " + row.targetVersetNumberEnd);
 	});
 
-	db.each("SELECT  rowid, chap_id, number, text FROM Versets WHERE chap_id = 2 AND number >= 5 AND number <=8 LIMIT 5", function(err, row) {
+	db.each("SELECT  rowid AS id, chapterId, number, text FROM Versets WHERE chapterId = 2 AND number >= 5 AND number <=8 LIMIT 5", function(err, row) {
 		if (row)
-		console.log(row.rowid + ": " + row.chap_id + ": " + row.number + ": " + row.text);
+		console.log(row.id + ": " + row.chapterId + ": " + row.number + ": " + row.text);
 	});
 
 
